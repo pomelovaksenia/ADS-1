7f403d10c370b33d83a3608e0a8c301d892cd03c
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-02-25 22:09:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,9 +25,19 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+   uint64_t searchValue = value + 1;
+        while (checkPrime(searchValue) == false){
+            searchValue++;
+        }
+        return searchValue;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+   uint64_t sum = 0;
+        for (int i = 2; i < hbound; i++){
+            if (pro(i) == true){
+                sum += i;
+            }
+        }
+        return sum;
 }
