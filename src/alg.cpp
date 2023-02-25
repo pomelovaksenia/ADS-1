deb028b6965c45b232447a20e44b7b23dedca0b5
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-02-25 22:29:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,12 +4,12 @@
 
 
 bool checkPrime(uint64_t value) {
-   for (uint64_t i = 2; i < value; i++) {
-       if ((value % i) == 0) {
-              return false;
-       }
-   }
-   return true;
+  for (uint64_t i = 2; i < value; i++) {
+      if ((value % i) == 0) {
+             return false;
+      }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -25,19 +25,19 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-   uint64_t searchValue = value + 1;
-        while (checkPrime(searchValue) == false) {
-            searchValue++;
-        }
-        return searchValue;
+  uint64_t searchValue = value + 1;
+       while (checkPrime(searchValue) == false) {
+           searchValue++;
+       }
+       return searchValue;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-   uint64_t sum = 0;
-        for (uint64_t i = 2; i < hbound; i++) {
-            if (checkPrime(i) == true) {
-                sum += i;
-            }
-        }
-        return sum;
+  uint64_t sum = 0;
+       for (uint64_t i = 2; i < hbound; i++) {
+           if (checkPrime(i) == true) {
+               sum += i;
+           }
+       }
+       return sum;
 }
