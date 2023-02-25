6cd8b5d8b8adb249bdc72f45b723a1e856f237ee
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-02-25 03:42:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,11 +4,24 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+   for (uint64_t i = 2; i < value; i++){
+       if ((value % i) == 0){
+              return false;
+       }
+   }
+   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+  uint64_t nowValue = 0;
+  int a = 1;
+  while (value != nowValue){
+    a++;
+    if (checkPrime(a) == true){
+       nowValue++;
+    }
+  }
+  return a;
 }
 
 uint64_t nextPrime(uint64_t value) {
